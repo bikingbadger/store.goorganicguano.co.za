63170c207e8bc8b84880f8c0443d4e6c7cb78a9e
Hilton Meyer <development@hiltonmeyer.com>
2021-02-26 21:44:58 +0200

    feat(prod): Create products component

---
+++ src/components/products/PopularProducts.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col sm="10" offset-sm="1" md="8" offset-md="2">
+        <h2>
+          Popular Products<v-btn
+            to="store"
+            class="primary--text text-lowercase"
+            text
+            >View All</v-btn
+          >
+        </h2>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col sm="10" offset-sm="1" md="8" offset-md="2">
+        <v-row>
+          <v-col sm="6" md="4">
+              Card
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
