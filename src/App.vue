cf8e5e670dc1895fe8f9f530d52b8c1d6ae5382e
Hilton Meyer <development@hiltonmeyer.com>
2021-02-26 20:25:48 +0200

    feat(route): Setup routes and views

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <v-app>
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+    </div> -->
+    <top-nav></top-nav>
+    <router-view />
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+import TopNav from '@/components/nav/TopNav.vue';
+export default {
+  data() {
+    return {
+      drawer: false,
+      items: [
+        { title: 'Home', link: '/', icon: 'home' },
+        { title: 'Store', link: 'store', icon: 'shopping-basket' },
+        { title: 'Cart', link: 'cart', icon: 'shopping-cart' },
+      ],
+    };
+  },
+  components: {
+    TopNav,
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,57 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/store',
+    name: 'store',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/Store.vue'),
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/Cart.vue'),
+  },
+  {
+    path: '/checkout',
+    name: 'checkout',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/Checkout.vue'),
+  },
+  {
+    path: '/thank-you',
+    name: 'thankyou',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/ThankYou.vue'),
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
