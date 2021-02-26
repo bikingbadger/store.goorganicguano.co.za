672c3fc209934b041669f377d1d8d22ccf6fbd92
Hilton Meyer <development@hiltonmeyer.com>
2021-02-26 21:12:36 +0200

    feat(home): Add hero section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>Home</div>
+  <main>
+    <header-hero></header-hero>
+    <popular-products></popular-products>
+  </main>
 </template>
 
 <script>
+import HeaderHero from '@/components/home/HeaderHero.vue';
+import PopularProducts from '@/components/products/PopularProducts.vue';
+
+
 export default {
   name: 'Home',
-  components: {},
+  components: { HeaderHero, PopularProducts },
 };
 </script>
