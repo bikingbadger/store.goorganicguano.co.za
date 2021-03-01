11603e8d60291f6f2efe76fa6bcb057c88c7d8ea
Hilton Meyer <development@hiltonmeyer.com>
2021-03-01 21:28:59 +0200

    feat(cart): Create checkout box

---
+++ src/components/cart/CheckoutBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card outlined>
+    <v-card-title>Checkout</v-card-title>
+    <v-card-text>
+      <v-text-field label="name" />
+      <v-text-field label="email" />
+      <p>Total: R100</p>
+      <v-btn color="primary" @click="checkout">Checkout</v-btn>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  methods: {
+    checkout() {
+      console.log('Checking out');
+    },
+  },
+};
+</script>
