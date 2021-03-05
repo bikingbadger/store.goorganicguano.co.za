e5f72ed9d9575a90d79e393f5c804e494d919158
Hilton Meyer <development@hiltonmeyer.com>
2021-03-05 19:47:32 +0200

    feat(checkout): Add stepper

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,3 +1,29 @@
 <template>
-  <div>Checkout</div>
+  <v-container>
+    <v-row>
+      <v-col sm="6" offset="3">
+        <v-stepper v-model="step">
+          <v-stepper-header>
+            <v-stepper-step step="1" :complete="step > 1">Contact Info</v-stepper-step>
+            <v-divider />
+            <v-stepper-step step="2" :complete="step > 2">Address Info</v-stepper-step>
+            <v-divider />
+            <v-stepper-step step="3">Done</v-stepper-step>
+          </v-stepper-header>
+
+          <v-stepper-items> </v-stepper-items>
+        </v-stepper>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      step: 2,
+    };
+  },
+};
+</script>
