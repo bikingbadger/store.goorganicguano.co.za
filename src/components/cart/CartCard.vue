eb54ad2412f292383f7cf13ff0f6fc917f8c51f5
Hilton Meyer <development@hiltonmeyer.com>
2021-03-05 21:07:59 +0200

    feat(products); Add stock images

--- src/components/cart/CartCard.vue
+++ src/components/cart/CartCard.vue
@@ -2,12 +2,12 @@
   <v-card outlined min-width="100%" class="mb-4 p-4">
     <div class="d-flex">
       <div>
-        <v-img :src="item.image" width="120px" height="120px"></v-img>
+        <v-img :src="require('@/assets/' + item.image)" width="120px" height="120px"></v-img>
       </div>
       <div class="d-flex flex-column justify-center align-start ">
         <v-card-title class="pt-2">{{ item.name }}</v-card-title>
         <v-card-subtitle>R{{ item.price }}</v-card-subtitle>
-        <!-- <v-card-text>{{ item.qty }}</v-card-text> -->
+        <v-card-text>Qty {{ item.qty }}</v-card-text>
         <v-btn color="error" outlined small class="ml-4"
           ><v-icon small left>mdi-minus</v-icon>Remove</v-btn
         >
